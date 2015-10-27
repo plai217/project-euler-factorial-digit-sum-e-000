df92b133ba6535bbe014d1aaddbcfa1e77b2ddb4
plai217 <philiplai217@gmail.com>
2015-10-27 14:51:31 -0400

    Done.

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,20 @@
 # code your solution here
+def factorial(num)
+  answer = 1.upto(num).inject {|answer,x| answer * x}
+end
+
+def sum_of_digits(num)
+  sum = 0
+  array = num.to_s.split("")
+  array.each do |x| 
+    sum += x.to_i
+  end
+  sum
+end
+
+
+def factorial_digit_sum(num)
+  factorial = factorial(num)
+  sum = sum_of_digits(factorial)
+  sum
+end
